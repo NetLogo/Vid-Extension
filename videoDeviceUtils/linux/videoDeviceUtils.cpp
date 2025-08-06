93da9f2c9e8efcbaa0a2403d698c161e56fd75c7
ERSUCC <isaacbrown5763@gmail.com>
2025-08-06 23:55:45 +0000

    Add camera detection library code for Linux

---
+++ videoDeviceUtils/linux/videoDeviceUtils.cpp
@@ -0,0 +1,69 @@
+#include "../videoDeviceUtils.h"
+
+#include <cstring>
+#include <fcntl.h>
+#include <filesystem>
+#include <sys/ioctl.h>
+#include <linux/videodev2.h>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+jobjectArray Java_org_nlogo_extensions_vid_util_VideoDeviceUtils_getDeviceNames(JNIEnv* env, jobject obj)
+{
+    // AVCaptureDeviceDiscoverySession* session = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes: @[AVCaptureDeviceTypeBuiltInWideAngleCamera, AVCaptureDeviceTypeExternal] mediaType:AVMediaTypeVideo position:AVCaptureDevicePositionUnspecified];
+
+    // NSArray<AVCaptureDevice*>* devices = [session devices];
+
+    // jobjectArray names = (jobjectArray)env->NewObjectArray((int)[devices count], env->FindClass("java/lang/String"), env->NewStringUTF(""));
+
+    // for (unsigned int i = 0; i < [devices count]; i++)
+    // {
+    //     env->SetObjectArrayElement(names, i, env->NewStringUTF([[devices[i] localizedName] cStringUsingEncoding: NSUTF8StringEncoding]));
+    // }
+
+    // return names;
+
+    std::vector<char*> names_vector;
+
+    for (const std::filesystem::directory_entry& entry : std::filesystem::directory_iterator({ "/dev" }))
+    {
+        if (!entry.is_directory() && std::strncmp(entry.path().filename().c_str(), "video", 5) == 0)
+        {
+            int fd = open(entry.path().c_str(), O_RDONLY);
+
+            if (fd == -1)
+            {
+                return nullptr;
+            }
+
+            v4l2_capability capability;
+
+            if (ioctl(fd, VIDIOC_QUERYCAP, &capability) != 0)
+            {
+                close(fd);
+
+                return nullptr;
+            }
+
+            close(fd);
+
+            char* name = (char*)malloc(sizeof(char) * 32);
+
+            strncpy(name, (char*)capability.card, 32);
+
+            names_vector.push_back(name);
+        }
+    }
+
+    jobjectArray names = (jobjectArray)env->NewObjectArray(names_vector.size(), env->FindClass("java/lang/String"), env->NewStringUTF(""));
+
+    for (unsigned int i = 0; i < names_vector.size(); i++)
+    {
+        env->SetObjectArrayElement(names, i, env->NewStringUTF(names_vector[i]));
+
+        free(names_vector[i]);
+    }
+
+    return names;
+}
