a10b82f3748940bc058ab2b7b50b86a77e122e89
ERSUCC <isaacbrown5763@gmail.com>
2026-08-11 11:19:05 -0500

    Bugfix: Prevent in-use exception when loaded in consecutive models

--- src/main/scala/util/VideoDeviceUtils.scala
+++ src/main/scala/util/VideoDeviceUtils.scala
@@ -23,7 +23,7 @@ object VideoDeviceUtils {
 
   // dynamic libraries must be loaded from the host filesystem, so first unpack the library
   // into a temporary directory before attempting to load it (Isaac B 8/5/25)
-  private val tempPath = Paths.get(System.getProperty("java.io.tmpdir"), s"videoDeviceUtils$ext")
+  private val tempPath = Files.createTempFile("videoDeviceUtils-", ext)
 
   Files.deleteIfExists(tempPath)
   Files.copy(getClass.getResourceAsStream(s"/lib/$dir/videoDeviceUtils$ext"), tempPath)
